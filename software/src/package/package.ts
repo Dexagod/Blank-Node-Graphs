424aede5aa23e880742a7daa4ffa6b5ec803af07
Ruben D. <ruben.dedecker@ugent.be>
2024-08-27 02:44:06 +0200

    fix: Fixed issue where graph renaming did not rename references

--- software/src/package/package.ts
+++ software/src/package/package.ts
@@ -1,4 +1,4 @@
-import { DataFactory, Quad_Graph, Quad_Object, Store } from "n3";
+import { BlankNode, DataFactory, NamedNode, Quad_Graph, Quad_Object, Store } from "n3";
 import { RDFContainsURI, RDFDatasetURI } from "../util/util";
 import { RDF } from "@inrupt/vocab-common-rdf";
 
@@ -29,12 +29,36 @@ export function createDatasetFromGraphsInStore( store: Store, graphTerms: Quad_G
     return { store, graph: containingGraphTerm, id: datasetSubject }
 }
 
-export function renameGraph( store: Store, source: Quad_Graph, target?: Quad_Graph, retainOriginal?: boolean  ) {
+/**
+ * renames graph for all quads containing the graph as the graph name, as well as all quads containing the graph as a subject or object value
+ * @param store 
+ * @param source 
+ * @param target 
+ * @param retainOriginal 
+ * @returns 
+ */
+export function renameGraph( store: Store, source: Quad_Graph, target?: NamedNode | BlankNode, retainOriginal?: boolean  ) {
     target = target || blankNode()
     retainOriginal = !!retainOriginal
+
+    // rename graph at graph position
     const matchingQuads = store.match(null, null, null, source)
     for (const matchedQuad of matchingQuads) {
         store.addQuad(quad(matchedQuad.subject, matchedQuad.predicate, matchedQuad.object, target))
+        if (!retainOriginal) store.removeQuad(matchedQuad)
+    }
+
+    // rename graph at object position
+    const matchingQuads2 = store.match(null, null, source, null)
+    for (const matchedQuad of matchingQuads2) {
+        store.addQuad(quad(matchedQuad.subject, matchedQuad.predicate, target, matchedQuad.graph))
+        if (!retainOriginal) store.removeQuad(matchedQuad)
+    }
+    
+    // rename graph at subject position
+    const matchingQuads3 = store.match(source, null, null, null)
+    for (const matchedQuad of matchingQuads3) {
+        store.addQuad(quad(target, matchedQuad.predicate, matchedQuad.object, matchedQuad.graph))
         if (!retainOriginal) store.removeQuad(matchedQuad)
     }
     return { store, graph: target}   
