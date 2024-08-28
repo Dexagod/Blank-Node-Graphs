67180b39c4857e1f84f24599c2daf3e60c13abfd
Ruben D. <ruben.dedecker@ugent.be>
2024-08-28 19:06:23 +0200

    fix: fixed script to run proxy server

--- server/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-npm install;
-npm run build;
-
-node dist/index.js -p 7846 -s http://xmlns.com/foaf/0.1/img http://xmlns.com/foaf/0.1/knows
---
+++ server/runProxy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+npx ts-node src/index.ts setup -p 7846 -s http://xmlns.com/foaf/0.1/img http://xmlns.com/foaf/0.1/knows
