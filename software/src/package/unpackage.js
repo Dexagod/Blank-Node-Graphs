f4eb32f574388dc36a10b9cc69ba8d9e703772d5
Ruben D. <ruben.dedecker@ugent.be>
2024-08-28 18:35:22 +0200

    fix: Fixed incorrectly compiled .js files being added

--- software/src/package/unpackage.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.unpackage = void 0;
-function unpackage() {
-}
-exports.unpackage = unpackage;
--- software/src/policy/validate.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validate = void 0;
-function validate() { }
-exports.validate = validate;
