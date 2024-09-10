bf171198d11f688ac334db0be6c15ad655ea1ffa
Ruben D. <ruben.dedecker@ugent.be>
2024-09-10 17:56:41 +0200

    Feat: Added pretty lists to serialized trig output

--- software/src/util/trigUtils.ts
+++ software/src/util/trigUtils.ts
@@ -1,21 +1,51 @@
-import { Parser, Quad, Store, Writer } from 'n3'
+import { Parser, Quad, Quad_Subject, Store, Writer, Quad_Object } from 'n3'
 import type * as rdf from 'rdf-js'
+import { DataFactory, unpackRDFList } from "../../src"
+import { RDF } from '@inrupt/vocab-common-rdf'
 
 export type TrigString = string
 export type TrigPackage = rdf.Quad[]
 export type TrigPackageString = TrigString
 
-export async function serializeTrigFromStore (store: Store): Promise<string> {
+export async function serializeTrigFromStore (store: Store, beautifyList?: boolean): Promise<string> {
+    
+    const listStore = new Store()
+    const listBases: Quad_Subject[] = []
+
     return await new Promise((resolve, reject) => {
         const writer = new Writer({ format: 'application/trig' })
-        writer.addQuads(store.getQuads(null, null, null, null))
+        for (let quad of store.getQuads(null, null, null, null)) {
+            if (beautifyList) {
+                if(quad.predicate.equals(DataFactory.namedNode(RDF.first))) {
+                    listStore.addQuad(quad)
+                    listBases.push(quad.subject)
+                } else if (quad.predicate.equals(DataFactory.namedNode(RDF.rest))) {
+                    listStore.addQuad(quad)
+                } else {
+                    writer.addQuad(quad)
+                }
+            } else {
+                writer.addQuad(quad)
+            }            
+        }
         writer.end((error, result) => {
             if (error) {
                 throw new Error('Could not serialize package string correctly')
             } else if (!result) {
                 resolve('')
             }
-            resolve(indentTrigString(result as string))
+            let trigString = indentTrigString(result as string)
+            console.log('before', trigString)
+            console.log()
+            if(beautifyList) {
+                console.log(listBases)
+                for (let base of listBases) {
+                    const unpacked = unpackRDFList(listStore, base)
+                    const regex = new RegExp((base.id || base.value)+"[^a-zA-Z0-9]")
+                    trigString = trigString.replace(regex, serializeRDFList(unpacked))
+                }
+            }            
+            resolve(trigString)
         })
     })
 }
@@ -43,4 +73,13 @@ function indentTrigString (trigString: TrigString): TrigString {
         }
     }
     return result.trimEnd()
+}
+
+function serializeRDFList(items: Quad_Object[]) {
+    let str = "("
+    for (let item of items) {
+        str += " "+(item.id || item.value).toString()
+    }
+    str += " )"
+    return str
 }
